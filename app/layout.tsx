35e290f548f03d25430e66adea59547394984e44
yaredow <yaredyilma11@gmail.com>
2024-11-28 14:58:10 +0000

    configure shadcn ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,10 @@
+import { Inter } from "next/font/google";
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -25,11 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`%=${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
